b668a235141fd0e2c2b9a3101842ee4366311c7b
Shalini <1705820@kiit.ac.in>
2020-10-14 12:30:53 +0530

    2nd commit

--- students/views.py
+++ students/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Student
-
+from .form import StudentDetailForm
 # Create your views here.
 def home(request):
-     context = {}
+     form = StudentDetailForm( request.POST or None)
+     if request.method == 'POST':
+          obj = form.save(commit=False)
+          obj.save()
+          return redirect(f'../')
+     context = {
+          'form' : form
+     }
      return render(request, 'home.html', context)
 
 def info(request):
